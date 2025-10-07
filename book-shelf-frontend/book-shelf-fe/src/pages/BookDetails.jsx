8ad71f1fde4f4c01f948897b6e71eb478f057d5e
edwardalx <eobkwaku@gmail.com>
2025-10-08 00:07:20 +0100

    resolved bookdetailPage refresh error

--- book-shelf-frontend/book-shelf-fe/src/pages/BookDetails.jsx
+++ book-shelf-frontend/book-shelf-fe/src/pages/BookDetails.jsx
@@ -1,40 +1,53 @@
-import React, { useEffect, useState } from 'react'
-import BookDetailCard from '../components/BookDetailCard'
-import { useBookStore } from '../zu-store/bookShelfStore';
-import { useParams } from 'react-router-dom';
-
+import React, { useEffect, useState } from "react";
+import BookDetailCard from "../components/BookDetailCard";
+import { useBookStore } from "../zu-store/bookShelfStore";
+import { useParams } from "react-router-dom";
 export default function BookDetails() {
-    const books = useBookStore((state) => state.books);
-    const {id} = useParams()
-    const [loading, setLoading] = useState(false)
-    const [error, setError] = useState(null)
-    const [filteredBook, setFilteredBook] = useState({})
+  const books = useBookStore((state) => state.books);
+  const { id } = useParams();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [filteredBook, setFilteredBook] = useState({});
 
-    useEffect(() => {
-   try {
-        setLoading(true)
-        if (!books){setError("No books available")}
-        let selectedBook = books.find(x => x.id === Number(id))
-        setFilteredBook(selectedBook)
-        
+  useEffect(() => {
+    try {
+      setLoading(true);
+      const storedBook = localStorage.getItem("selectedBook");
+      if ((!storedBook || storedBook === "{}") && books.length === 0) {
+        setError("No books in store");
+      }
+      if (books.length === 0) {
+        setFilteredBook(JSON.parse(storedBook));
+      } else {
+        if (books.length !== 0) {
+          let selectedBook = books.find((x) => x.id === Number(id));
+          setFilteredBook(selectedBook);
+          console.log("Filterbook is", Object.keys(filteredBook).length !==0? filteredBook:selectedBook);
+          localStorage.setItem(
+            "selectedBook",
+            JSON.stringify(selectedBook ? selectedBook : filteredBook)
+          );
+        }
+      }
     } catch (error) {
-        console.error(error);
-        
+      setError("Something went wrong");
+      console.error(error);
+    } finally {
+      setLoading(false);
     }
-    finally{
-        setLoading(false)
-    }
-    
+  }, [id, books]);
 
-    }, [id])
-    
-    
   return (
     <div>
-        {error && <p className='text-red-600'>❗{error}</p>}
-        {loading && <p className='text-green-600'> <span>☸️ </span>Loading ... </p>}
-        <BookDetailCard book={filteredBook}/>
-        {console.log("Book detail of :", filteredBook)}
+      {error && <p className="text-red-600">❗{error}</p>}
+      {loading && (
+        <p className="text-green-600">
+          {" "}
+          <span>☸️ </span>Loading ...{" "}
+        </p>
+      )}
+      <BookDetailCard book={filteredBook} />
+      {console.log("Book detail of :", filteredBook)}
     </div>
-  )
+  );
 }
